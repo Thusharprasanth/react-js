fcaf849ee92c046d1728cf3674014f1d5fed0258
Thusharprasanth <thusharprasanth@gmail.com>
2020-09-12 22:13:50 +0530

    Updates

---
+++ precache-manifest.c6289688d32f2c5c994a97a852e1afef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc207e86e7ad3801c731e2986f7c117",
+    "url": "/react-js/index.html"
+  },
+  {
+    "revision": "35a43ab4141662926c01",
+    "url": "/react-js/static/css/2.06c478a6.chunk.css"
+  },
+  {
+    "revision": "35a43ab4141662926c01",
+    "url": "/react-js/static/js/2.fc17d54b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-js/static/js/2.fc17d54b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ecd9c08ffeb797d540",
+    "url": "/react-js/static/js/main.b240d2d7.chunk.js"
+  },
+  {
+    "revision": "9f10ba67babb2911fcbc",
+    "url": "/react-js/static/js/runtime-main.3629446c.js"
+  }
+]);
